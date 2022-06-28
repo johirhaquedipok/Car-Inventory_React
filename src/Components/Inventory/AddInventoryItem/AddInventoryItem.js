2cca31902c6d8ea8c60f993a405b8202e8bf2dba
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-06-28 22:03:10 +0600

    style updateed

--- src/Components/Inventory/AddInventoryItem/AddInventoryItem.js
+++ src/Components/Inventory/AddInventoryItem/AddInventoryItem.js
@@ -44,171 +44,184 @@ const AddInventoryItem = () => {
     reset();
   };
   return (
-    <Row>
-      <Col md={6}>
-        <Image
-          fluid
-          src="https://imgd-ct.aeplcdn.com/664x415/n/cw/ec/45849/tigor-ev-exterior-left-front-three-quarter-14.jpeg?isig=0&q=75"
-          alt="carimage"
-        />
-      </Col>
-      <Col md={6}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-3">
-            <label className="form-label" htmlFor="name">
-              Car Name{" "}
-            </label>
+    <>
+      <div className="text-center my-4">
+        <h4>Add Some data</h4>
+        <div className=" d-flex align-items-cetner justify-content-center">
+          <div
+            className="bg-dark mb-3"
+            style={{ height: "5px", width: "80px", borderRadius: "10px" }}
+          ></div>
+        </div>
+        <p className="text-info">Make sure all fields are filled</p>
+      </div>
 
-            {/* use aria-invalid to indicate field contain error */}
-            <input
-              className="form-control"
-              id="name"
-              aria-invalid={errors.name ? "true" : "false"}
-              {...register("name", { required: true, maxLength: 30 })}
-            />
+      <Row>
+        <Col md={6}>
+          <Image
+            fluid
+            src="https://images.unsplash.com/photo-1514316454349-750a7fd3da3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+            alt="carimage"
+          />
+        </Col>
+        <Col md={6}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="name">
+                Car Name{" "}
+              </label>
 
-            {/* use role="alert" to announce the error message */}
-            {errors.name && errors.name.type === "required" && (
-              <span className="text-danger" role="alert">
-                This is required
-              </span>
-            )}
-            {errors.name && errors.name.type === "maxLength" && (
-              <span className="text-danger" role="alert">
-                Max length exceeded
-              </span>
-            )}
-          </div>
-          {/* car price */}
-          <div className="mb-3">
-            <label className="form-label" htmlFor="name">
-              Car Price{" "}
-            </label>
+              {/* use aria-invalid to indicate field contain error */}
+              <input
+                className="form-control"
+                id="name"
+                aria-invalid={errors.name ? "true" : "false"}
+                {...register("name", { required: true, maxLength: 30 })}
+              />
 
-            {/* use aria-invalid to indicate field contain error */}
-            <input
-              className="form-control"
-              id="price"
-              aria-invalid={errors.price ? "true" : "false"}
-              {...register("price", { required: true, maxLength: 30 })}
-            />
+              {/* use role="alert" to announce the error message */}
+              {errors.name && errors.name.type === "required" && (
+                <span className="text-danger" role="alert">
+                  This is required
+                </span>
+              )}
+              {errors.name && errors.name.type === "maxLength" && (
+                <span className="text-danger" role="alert">
+                  Max length exceeded
+                </span>
+              )}
+            </div>
+            {/* car price */}
+            <div className="mb-3">
+              <label className="form-label" htmlFor="name">
+                Car Price{" "}
+              </label>
 
-            {/* use role="alert" to announce the error message */}
-            {errors.price && errors.price.type === "required" && (
-              <span className="text-danger" role="alert">
-                This is required
-              </span>
-            )}
-            {errors.price && errors.price.type === "maxLength" && (
-              <span className="text-danger" role="alert">
-                Max length exceeded
-              </span>
-            )}
-          </div>
-          {/* car quantity */}
-          <div className="mb-3">
-            <label className="form-label" htmlFor="quantity">
-              Car Quantity{" "}
-            </label>
+              {/* use aria-invalid to indicate field contain error */}
+              <input
+                className="form-control"
+                id="price"
+                aria-invalid={errors.price ? "true" : "false"}
+                {...register("price", { required: true, maxLength: 30 })}
+              />
 
-            {/* use aria-invalid to indicate field contain error */}
-            <input
-              className="form-control"
-              id="quantity"
-              aria-invalid={errors.quantity ? "true" : "false"}
-              {...register("quantity", { required: true, maxLength: 30 })}
-            />
+              {/* use role="alert" to announce the error message */}
+              {errors.price && errors.price.type === "required" && (
+                <span className="text-danger" role="alert">
+                  This is required
+                </span>
+              )}
+              {errors.price && errors.price.type === "maxLength" && (
+                <span className="text-danger" role="alert">
+                  Max length exceeded
+                </span>
+              )}
+            </div>
+            {/* car quantity */}
+            <div className="mb-3">
+              <label className="form-label" htmlFor="quantity">
+                Car Quantity{" "}
+              </label>
 
-            {/* use role="alert" to announce the error message */}
-            {errors.quantity && errors.quantity.type === "required" && (
-              <span className="text-danger" role="alert">
-                This is required
-              </span>
-            )}
-            {errors.quantity && errors.quantity.type === "maxLength" && (
-              <span className="text-danger" role="alert">
-                Max length exceeded
-              </span>
-            )}
-          </div>
-          {/* car supllierName */}
-          <div className="mb-3">
-            <label className="form-label" htmlFor="supllierName">
-              Car supllierName{" "}
-            </label>
+              {/* use aria-invalid to indicate field contain error */}
+              <input
+                className="form-control"
+                id="quantity"
+                aria-invalid={errors.quantity ? "true" : "false"}
+                {...register("quantity", { required: true, maxLength: 30 })}
+              />
 
-            {/* use aria-invalid to indicate field contain error */}
-            <input
-              className="form-control"
-              id="supllierName"
-              aria-invalid={errors.supllierName ? "true" : "false"}
-              {...register("supllierName", { required: true, maxLength: 30 })}
-            />
+              {/* use role="alert" to announce the error message */}
+              {errors.quantity && errors.quantity.type === "required" && (
+                <span className="text-danger" role="alert">
+                  This is required
+                </span>
+              )}
+              {errors.quantity && errors.quantity.type === "maxLength" && (
+                <span className="text-danger" role="alert">
+                  Max length exceeded
+                </span>
+              )}
+            </div>
+            {/* car supllierName */}
+            <div className="mb-3">
+              <label className="form-label" htmlFor="supllierName">
+                Car supllierName{" "}
+              </label>
 
-            {/* use role="alert" to announce the error message */}
-            {errors.supllierName && errors.supllierName.type === "required" && (
-              <span className="text-danger" role="alert">
-                This is required
-              </span>
-            )}
-            {errors.supllierName && errors.supllierName.type === "maxLength" && (
-              <span className="text-danger" role="alert">
-                Max length exceeded
-              </span>
-            )}
-          </div>
-          {/* car Image */}
-          <div className="mb-3">
-            <label className="form-label" htmlFor="img">
-              Car img link
-            </label>
+              {/* use aria-invalid to indicate field contain error */}
+              <input
+                className="form-control"
+                id="supllierName"
+                aria-invalid={errors.supllierName ? "true" : "false"}
+                {...register("supllierName", { required: true, maxLength: 30 })}
+              />
 
-            {/* use aria-invalid to indicate field contain error */}
-            <input
-              className="form-control"
-              id="img"
-              aria-invalid={errors.img ? "true" : "false"}
-              {...register("img", { required: true })}
-            />
+              {/* use role="alert" to announce the error message */}
+              {errors.supllierName && errors.supllierName.type === "required" && (
+                <span className="text-danger" role="alert">
+                  This is required
+                </span>
+              )}
+              {errors.supllierName && errors.supllierName.type === "maxLength" && (
+                <span className="text-danger" role="alert">
+                  Max length exceeded
+                </span>
+              )}
+            </div>
+            {/* car Image */}
+            <div className="mb-3">
+              <label className="form-label" htmlFor="img">
+                Car img link
+              </label>
 
-            {/* use role="alert" to announce the error message */}
-            {errors.img && errors.img.type === "required" && (
-              <span className="text-danger" role="alert">
-                This is required
-              </span>
-            )}
-          </div>
+              {/* use aria-invalid to indicate field contain error */}
+              <input
+                className="form-control"
+                id="img"
+                aria-invalid={errors.img ? "true" : "false"}
+                {...register("img", { required: true })}
+              />
 
-          {/* car Description */}
-          <div className="mb-3">
-            <label className="form-label" htmlFor="description">
-              Car Description{" "}
-            </label>
+              {/* use role="alert" to announce the error message */}
+              {errors.img && errors.img.type === "required" && (
+                <span className="text-danger" role="alert">
+                  This is required
+                </span>
+              )}
+            </div>
 
-            {/* use aria-invalid to indicate field contain error */}
-            <input
-              className="form-control"
-              id="description"
-              aria-invalid={errors.description ? "true" : "false"}
-              {...register("description", { required: true, maxLength: 200 })}
-            />
+            {/* car Description */}
+            <div className="mb-3">
+              <label className="form-label" htmlFor="description">
+                Car Description{" "}
+              </label>
 
-            {/* use role="alert" to announce the error message */}
-            {errors.description && errors.description.type === "required" && (
-              <span className="text-danger" role="alert">
-                This is required
-              </span>
-            )}
-            {errors.description && errors.description.type === "maxLength" && (
-              <span className="text-danger" role="alert">
-                Max length exceeded
-              </span>
-            )}
-          </div>
-          <input className="form-control" type="submit" />
-        </form>
-      </Col>
-    </Row>
+              {/* use aria-invalid to indicate field contain error */}
+              <input
+                className="form-control"
+                id="description"
+                aria-invalid={errors.description ? "true" : "false"}
+                {...register("description", { required: true, maxLength: 200 })}
+              />
+
+              {/* use role="alert" to announce the error message */}
+              {errors.description && errors.description.type === "required" && (
+                <span className="text-danger" role="alert">
+                  This is required
+                </span>
+              )}
+              {errors.description && errors.description.type === "maxLength" && (
+                <span className="text-danger" role="alert">
+                  Max length exceeded
+                </span>
+              )}
+            </div>
+            <input className="form-control" type="submit" />
+          </form>
+        </Col>
+      </Row>
+    </>
   );
 };
 
