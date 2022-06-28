48506c3c47d6655006abf25c2bac825d4bddee9d
johirhaquedipok <johirhaquedipok2@gmail.com>
2022-06-28 08:51:24 +0600

    update delete from users db

--- src/Components/Inventory/MyItem/MyItem.js
+++ src/Components/Inventory/MyItem/MyItem.js
@@ -21,19 +21,38 @@ const MyItem = () => {
       .then((res) => res.json())
       .then((newData) => setCars(newData));
   }, []);
-
+  console.log(cars);
   useEffect(() => {
     fetch(`http://localhost:5000/userInventory?email=${email}`)
       .then((res) => res.json())
-      .then((data) => getProducts(data[0]));
+      .then((data) => {
+        if (data.length === 0) {
+          console.log(data);
+        } else {
+          getProducts(data[0]);
+        }
+      });
   }, [email, getProducts]);
-
   const handleRemove = (id) => {
     const sure = window.confirm("Are you sure!");
 
     if (sure) {
-      const url = `http://localhost:5000/inventory/${id}`;
-      fetch(url, {
+      // remove from database inventory
+      const userData = { email: email, productId: id };
+      fetch(`http://localhost:5000/userInventory?email=${email}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+      // remove from main invetory
+
+      fetch(`http://localhost:5000/inventory/${id}`, {
         method: "DELETE",
       })
         .then((res) => res.json())
@@ -45,39 +64,43 @@ const MyItem = () => {
   };
   return (
     <Row>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Car Name</th>
-            <th>Car Price</th>
-            <th>Car Quantity</th>
-            <th>Car Supplier</th>
-            <th>Remove</th>
-          </tr>
-        </thead>
-
-        {cars.map((car, idx) => (
-          <tbody key={car._id}>
+      {cars.length !== 0 ? (
+        <Table striped bordered hover>
+          <thead>
             <tr>
-              <td>{idx + 1}</td>
-              <td>{car?.name}</td>
-              <td>{car?.price}</td>
-              <td>{car?.quantity}</td>
-              <td>{car?.supllierName}</td>
-              <td>
-                <Button
-                  variant="danger"
-                  className="d-flex align-items-center justif-content-center"
-                  onClick={() => handleRemove(car?._id)}
-                >
-                  <BsFillXCircleFill />
-                </Button>
-              </td>
+              <th>#</th>
+              <th>Car Name</th>
+              <th>Car Price</th>
+              <th>Car Quantity</th>
+              <th>Car Supplier</th>
+              <th>Remove</th>
             </tr>
-          </tbody>
-        ))}
-      </Table>
+          </thead>
+
+          {cars.map((car, idx) => (
+            <tbody key={car._id}>
+              <tr>
+                <td>{idx + 1}</td>
+                <td>{car?.name}</td>
+                <td>{car?.price}</td>
+                <td>{car?.quantity}</td>
+                <td>{car?.supllierName}</td>
+                <td>
+                  <Button
+                    variant="danger"
+                    className="d-flex align-items-center justif-content-center"
+                    onClick={() => handleRemove(car?._id)}
+                  >
+                    <BsFillXCircleFill />
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </Table>
+      ) : (
+        <div className="align-self-center">You got no data </div>
+      )}
     </Row>
   );
 };
